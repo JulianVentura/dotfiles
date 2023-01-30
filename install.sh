9c614fd2baa5a193d5e1a17df5b3e623ad8ddda8
Julian Ventura <jventura@fi.uba.ar>
2023-01-30 19:39:32 -0300

    Modify docker installation

--- install.sh
+++ install.sh
@@ -71,7 +71,6 @@ sudo systemctl enable lightdm
 
 #docker
 sudo systemctl start docker.service
-sudo systemctl status docker.service 
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 
