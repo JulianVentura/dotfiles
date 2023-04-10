6d5e3d6d5aab3c92797f9158fad10fffe28a47a0
Julian Ventura <jventura@fi.uba.ar>
2023-04-10 00:27:59 -0300

    Modify nvim configurations

--- nvim/lua/julian/plugins/lsp/lspsaga.lua
+++ nvim/lua/julian/plugins/lsp/lspsaga.lua
@@ -8,12 +8,27 @@ saga.setup({
   -- keybinds for navigation in lspsaga window
   scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
   -- use enter to open file with definition preview
-  definition = {
-    edit = "<CR>",
-  },
+  request_timeout = 2000,
+  -- definition = {
+  --   edit = "<CR>",
+  -- },
   ui = {
     colors = {
       normal_bg = "#022746",
     },
   },
+  -- For default options for each command, see below
+  finder = {
+    keys = {
+      jump_to = "p",
+      expand_or_jump = "<CR>",
+      vsplit = "v",
+      split = "s",
+      tabe = "t",
+      tabnew = "r",
+      quit = { "q", "<ESC>" },
+      close_in_preview = "<ESC>",
+    },
+  },
+  code_action = { ... },
 })
