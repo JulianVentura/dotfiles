9f1ece39a0c24d8741db0c275eb1d551d109e920
Julian Ventura <jventura@fi.uba.ar>
2024-02-17 13:51:23 -0300

    Modify nvim themes logic

--- common/nvim/lua/julian/core/colorscheme.lua
+++ common/nvim/lua/julian/core/colorscheme.lua
@@ -1,7 +1,146 @@
--- set colorscheme to nightfly with protected call
--- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
-if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
+Theme = "gruvbox"
+
+-- Nightfly
+
+local nightfly = function()
+  local status, _ = pcall(require, "gruvbox")
+  if not status then
+    print("Error: Couldn't load gruvbox theme")
+    return false
+  end
+
+  -- Lua initialization file
+  vim.g.nightflyCursorColor = false
+  -- Lua initialization file
+  vim.g.nightflyItalics = true
+  -- Lua initialization file
+  vim.g.nightflyNormalFloat = false
+  -- Lua initialization file
+  vim.g.nightflyTerminalColors = true
+  -- Lua initialization file
+  vim.g.nightflyTransparent = false
+  -- Lua initialization file
+  vim.g.nightflyUndercurls = true
+  -- Lua initialization file
+  vim.g.nightflyUnderlineMatchParen = false
+  -- Lua initialization file
+  vim.g.nightflyVirtualTextColor = false
+  -- Lua initialization file
+  vim.g.nightflyWinSeparator = 1
+
+  return true
 end
+
+-- Gruvbox
+local gruvbox = function()
+  local status, theme = pcall(require, "gruvbox")
+  if not status then
+    print("Error: Couldn't load gruvbox theme")
+    return false
+  end
+
+  theme.setup({
+    terminal_colors = true,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  })
+
+  return true
+end
+
+-- VSCode
+
+local function vscode()
+  local status, theme = pcall(require, "vscode")
+  if not status then
+    print("Error: Couldn't load vscode theme")
+    return false
+  end
+
+  local colors = nil
+  status, colors = pcall(require, "vscode.colors")
+  if not status then
+    print("Error: Couldn't load vscode colors")
+    return false
+  end
+
+  local c = colors.get_colors()
+
+  theme.setup({
+    -- Alternatively set style in setup
+    -- style = 'light'
+
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    -- color_overrides = {
+    --   vscLineNumber = "#FFFFFF",
+    -- },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+      -- this supports the same val table as vim.api.nvim_set_hl
+      -- use colors from this colorscheme by requiring vscode.colors!
+      Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+    },
+  })
+
+  theme.load()
+
+  return true
+end
+
+local function configure_theme(theme)
+  local themes = {
+    gruvbox = gruvbox,
+    nightfly = nightfly,
+    vscode = vscode,
+  }
+
+  if not themes[theme] then
+    print("Error: Theme " .. theme .. " not available")
+    return
+  end
+
+  if not themes[theme]() then
+    return
+  end
+
+  local status, _ = pcall(function(_)
+    return vim.cmd("colorscheme " .. theme)
+  end)
+
+  if not status then
+    print("Error: Couldn't set colorscheme " .. theme)
+  end
+end
+
+configure_theme(Theme)
+
+return Theme
--- common/nvim/lua/julian/plugins/lualine.lua
+++ common/nvim/lua/julian/plugins/lualine.lua
@@ -1,36 +1,77 @@
+local theme = require("julian.core.colorscheme")
+
 -- import lualine plugin safely
 local status, lualine = pcall(require, "lualine")
 if not status then
   return
 end
 
--- get lualine nightfly theme
-local lualine_nightfly = require("lualine.themes.nightfly")
+-- Configure theme
 
--- new colors for theme
-local new_colors = {
-  blue = "#65D1FF",
-  green = "#3EFFDC",
-  violet = "#FF61EF",
-  yellow = "#FFDA7B",
-  black = "#000000",
-}
+-- Gruvbox setup function
+local function gruvbox()
+  lualine.setup({
+    options = {
+      theme = "gruvbox",
+    },
+  })
+end
 
--- change nightlfy theme colors
-lualine_nightfly.normal.a.bg = new_colors.blue
-lualine_nightfly.insert.a.bg = new_colors.green
-lualine_nightfly.visual.a.bg = new_colors.violet
-lualine_nightfly.command = {
-  a = {
-    gui = "bold",
-    bg = new_colors.yellow,
-    fg = new_colors.black, -- black
-  },
-}
+-- Nightfly setup function
+local function nightfly()
+  -- get lualine nightfly theme
+  local lualine_nightfly = require("lualine.themes.nightfly")
 
--- configure lualine with modified theme
-lualine.setup({
-  options = {
-    theme = lualine_nightfly,
-  },
-})
+  -- new colors for theme
+  local new_colors = {
+    blue = "#65D1FF",
+    green = "#3EFFDC",
+    violet = "#FF61EF",
+    yellow = "#FFDA7B",
+    black = "#000000",
+  }
+
+  -- change nightlfy theme colors
+  lualine_nightfly.normal.a.bg = new_colors.blue
+  lualine_nightfly.insert.a.bg = new_colors.green
+  lualine_nightfly.visual.a.bg = new_colors.violet
+  lualine_nightfly.command = {
+    a = {
+      gui = "bold",
+      bg = new_colors.yellow,
+      fg = new_colors.black, -- black
+    },
+  }
+
+  lualine.setup({
+    options = {
+      theme = lualine_nightfly,
+    },
+  })
+end
+
+local function vscode()
+  lualine.setup({
+    options = {
+      theme = "material",
+    },
+  })
+end
+
+-- Configuration auxiliar function
+local function configure_lualine(selected_theme)
+  local themes = {
+    gruvbox = gruvbox,
+    nightfly = nightfly,
+    vscode = vscode,
+  }
+
+  if not themes[selected_theme] then
+    print("Error: Invalid theme for lualine")
+    return nil
+  end
+
+  themes[selected_theme]()
+end
+
+configure_lualine(theme)
